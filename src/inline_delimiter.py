5b29f199129f4eacf9bc91b971c5323d28381b69
Rishan-Jadva <rishanjadva@gmail.com>
2025-05-05 19:46:19 +1000

    splits text into links and images

--- src/inline_delimiter.py
+++ src/inline_delimiter.py
@@ -23,3 +23,52 @@ def extract_markdown_images(text):
 
 def extract_markdown_links(text):
     return re.findall(r"(?<!!)\[(.*?)\]\((.*?)\)", text)
+
+def split_nodes_image(old_nodes):
+    new_nodes = []
+    for node in old_nodes:
+        if node.text_type != TextType.TEXT:
+            new_nodes.append(node)
+            continue
+        extracted_tuple = extract_markdown_images(node.text)
+        if len(extracted_tuple) == 0:
+            new_nodes.append(node)
+            continue
+
+        temp_text = node.text
+        for alt, url in extracted_tuple:
+            split_str = f"![{alt}]({url})"
+            sections = temp_text.split(split_str, 1)
+            if sections[0] != "":
+                new_nodes.append(TextNode(sections[0], TextType.TEXT))
+            new_nodes.append(TextNode(alt, TextType.IMAGE, url))
+            temp_text = sections[1]
+        if temp_text != "":
+            new_nodes.append(TextNode(temp_text, TextType.TEXT))
+    return new_nodes
+
+def split_nodes_link(old_nodes):
+    new_nodes = []
+    for node in old_nodes:
+        if node.text_type != TextType.TEXT:
+            new_nodes.append(node)
+            continue
+        extracted_tuple = extract_markdown_links(node.text)
+        if len(extracted_tuple) == 0:
+            new_nodes.append(node)
+            continue
+
+        temp_text = node.text
+        for text, url in extracted_tuple:
+            split_str = f"[{text}]({url})"
+            sections = temp_text.split(split_str, 1)
+            if sections[0] != "":
+                new_nodes.append(TextNode(sections[0], TextType.TEXT))
+            new_nodes.append(TextNode(text, TextType.LINK, url))
+            temp_text = sections[1]
+        if temp_text != "":
+            new_nodes.append(TextNode(temp_text, TextType.TEXT))
+    return new_nodes
+
+
+                
--- src/test_inline_delimiter.py
+++ src/test_inline_delimiter.py
@@ -1,6 +1,6 @@
 import unittest
 
-from inline_delimiter import split_nodes_delimiter, extract_markdown_images, extract_markdown_links
+from inline_delimiter import split_nodes_delimiter, extract_markdown_images, extract_markdown_links, split_nodes_image, split_nodes_link
 from textnode import TextNode, TextType
 
 class TestTextNode(unittest.TestCase):
@@ -59,6 +59,208 @@ class TestTextNode(unittest.TestCase):
         self.assertListEqual([
             ("link text", "link url text")
         ], matches)
+
+    def test_split_nodes_image_case1(self):
+        node = TextNode("whatever ![alt](url)", TextType.TEXT)
+        self.assertListEqual([
+            TextNode("whatever ", TextType.TEXT),
+            TextNode("alt", TextType.IMAGE, "url")
+        ], split_nodes_image([node]))
+
+    def test_split_nodes_image_case2(self):
+        node = TextNode("![alt](url) whatever", TextType.TEXT)
+        self.assertListEqual([
+            TextNode("alt", TextType.IMAGE, "url"),
+            TextNode(" whatever", TextType.TEXT)
+        ], split_nodes_image([node]))
+
+    def test_split_nodes_image_case3(self):
+        node = TextNode("![alt](url)", TextType.TEXT)
+        self.assertListEqual([
+            TextNode("alt", TextType.IMAGE, "url")
+        ], split_nodes_image([node]))
+
+    def test_split_nodes_image_case4(self):
+        node = TextNode("whatever", TextType.TEXT)
+        self.assertListEqual([
+            TextNode("whatever", TextType.TEXT)
+        ], split_nodes_image([node]))
+
+    def test_split_nodes_image_case5(self):
+        node = TextNode("whatever ![alt](url) whatever", TextType.TEXT)
+        self.assertListEqual([
+            TextNode("whatever ", TextType.TEXT),
+            TextNode("alt", TextType.IMAGE, "url"),
+            TextNode(" whatever", TextType.TEXT)
+        ], split_nodes_image([node]))
+
+    def test_split_nodes_image_case6(self):
+        node = TextNode("![alt](url) whatever ![alt2](url2)", TextType.TEXT)
+        self.assertListEqual([
+            TextNode("alt", TextType.IMAGE, "url"),
+            TextNode(" whatever ", TextType.TEXT),
+            TextNode("alt2", TextType.IMAGE, "url2")
+        ], split_nodes_image([node]))
+
+    def test_split_nodes_image_case7(self):
+        node = TextNode("![alt](url)![alt2](url2)", TextType.TEXT)
+        self.assertListEqual([
+            TextNode("alt", TextType.IMAGE, "url"),
+            TextNode("alt2", TextType.IMAGE, "url2")
+        ], split_nodes_image([node]))
+
+    def test_split_nodes_image_case8(self):
+        node = TextNode("whateverwhatever", TextType.TEXT)
+        self.assertListEqual([
+            TextNode("whateverwhatever", TextType.TEXT)
+        ], split_nodes_image([node]))
+
+    def test_split_nodes_image_case9(self):
+        node = TextNode("![alt](url)![alt2](url2)whatever", TextType.TEXT)
+        self.assertListEqual([
+            TextNode("alt", TextType.IMAGE, "url"),
+            TextNode("alt2", TextType.IMAGE, "url2"),
+            TextNode("whatever", TextType.TEXT)
+        ], split_nodes_image([node]))
+
+    def test_split_nodes_image_case10(self):
+        node = TextNode("whatever![alt](url)![alt2](url2)", TextType.TEXT)
+        self.assertListEqual([
+            TextNode("whatever", TextType.TEXT),
+            TextNode("alt", TextType.IMAGE, "url"),
+            TextNode("alt2", TextType.IMAGE, "url2")
+        ], split_nodes_image([node]))
+
+    def test_split_nodes_image_case11(self):
+        node = TextNode("![alt](url)whatever", TextType.TEXT)
+        self.assertListEqual([
+            TextNode("alt", TextType.IMAGE, "url"),
+            TextNode("whatever", TextType.TEXT)
+        ], split_nodes_image([node]))
+
+    def test_split_nodes_image_case12(self):
+        node = TextNode("whatever![alt](url)", TextType.TEXT)
+        self.assertListEqual([
+            TextNode("whatever", TextType.TEXT),
+            TextNode("alt", TextType.IMAGE, "url")
+        ], split_nodes_image([node]))
+
+    def test_split_nodes_image_case13(self):
+        node = TextNode("![alt](url)whatever![alt2](url2)", TextType.TEXT)
+        self.assertListEqual([
+            TextNode("alt", TextType.IMAGE, "url"),
+            TextNode("whatever", TextType.TEXT),
+            TextNode("alt2", TextType.IMAGE, "url2")
+        ], split_nodes_image([node]))
+
+    def test_split_nodes_image_case14(self):
+        node = TextNode("whatever![alt](url)whatever", TextType.TEXT)
+        self.assertListEqual([
+            TextNode("whatever", TextType.TEXT),
+            TextNode("alt", TextType.IMAGE, "url"),
+            TextNode("whatever", TextType.TEXT)
+        ], split_nodes_image([node]))
+
+    def test_split_nodes_link_case1(self):
+        node = TextNode("whatever [text](url)", TextType.TEXT)
+        self.assertListEqual([
+            TextNode("whatever ", TextType.TEXT),
+            TextNode("text", TextType.LINK, "url")
+        ], split_nodes_link([node]))
+
+    def test_split_nodes_link_case2(self):
+        node = TextNode("[text](url) whatever", TextType.TEXT)
+        self.assertListEqual([
+            TextNode("text", TextType.LINK, "url"),
+            TextNode(" whatever", TextType.TEXT)
+        ], split_nodes_link([node]))
+
+    def test_split_nodes_link_case3(self):
+        node = TextNode("[text](url)", TextType.TEXT)
+        self.assertListEqual([
+            TextNode("text", TextType.LINK, "url")
+        ], split_nodes_link([node]))
+
+    def test_split_nodes_link_case4(self):
+        node = TextNode("whatever", TextType.TEXT)
+        self.assertListEqual([
+            TextNode("whatever", TextType.TEXT)
+        ], split_nodes_link([node]))
+
+    def test_split_nodes_link_case5(self):
+        node = TextNode("whatever [text](url) whatever", TextType.TEXT)
+        self.assertListEqual([
+            TextNode("whatever ", TextType.TEXT),
+            TextNode("text", TextType.LINK, "url"),
+            TextNode(" whatever", TextType.TEXT)
+        ], split_nodes_link([node]))
+
+    def test_split_nodes_link_case6(self):
+        node = TextNode("[text](url) whatever [text2](url2)", TextType.TEXT)
+        self.assertListEqual([
+            TextNode("text", TextType.LINK, "url"),
+            TextNode(" whatever ", TextType.TEXT),
+            TextNode("text2", TextType.LINK, "url2")
+        ], split_nodes_link([node]))
+
+    def test_split_nodes_link_case7(self):
+        node = TextNode("[text](url)[text2](url2)", TextType.TEXT)
+        self.assertListEqual([
+            TextNode("text", TextType.LINK, "url"),
+            TextNode("text2", TextType.LINK, "url2")
+        ], split_nodes_link([node]))
+
+    def test_split_nodes_link_case8(self):
+        node = TextNode("whateverwhatever", TextType.TEXT)
+        self.assertListEqual([
+            TextNode("whateverwhatever", TextType.TEXT)
+        ], split_nodes_link([node]))
+
+    def test_split_nodes_link_case9(self):
+        node = TextNode("[text](url)[text2](url2)whatever", TextType.TEXT)
+        self.assertListEqual([
+            TextNode("text", TextType.LINK, "url"),
+            TextNode("text2", TextType.LINK, "url2"),
+            TextNode("whatever", TextType.TEXT)
+        ], split_nodes_link([node]))
+
+    def test_split_nodes_link_case10(self):
+        node = TextNode("whatever[text](url)[text2](url2)", TextType.TEXT)
+        self.assertListEqual([
+            TextNode("whatever", TextType.TEXT),
+            TextNode("text", TextType.LINK, "url"),
+            TextNode("text2", TextType.LINK, "url2")
+        ], split_nodes_link([node]))
+
+    def test_split_nodes_link_case11(self):
+        node = TextNode("[text](url)whatever", TextType.TEXT)
+        self.assertListEqual([
+            TextNode("text", TextType.LINK, "url"),
+            TextNode("whatever", TextType.TEXT)
+        ], split_nodes_link([node]))
+
+    def test_split_nodes_link_case12(self):
+        node = TextNode("whatever[text](url)", TextType.TEXT)
+        self.assertListEqual([
+            TextNode("whatever", TextType.TEXT),
+            TextNode("text", TextType.LINK, "url")
+        ], split_nodes_link([node]))
+
+    def test_split_nodes_link_case13(self):
+        node = TextNode("[text](url)whatever[text2](url2)", TextType.TEXT)
+        self.assertListEqual([
+            TextNode("text", TextType.LINK, "url"),
+            TextNode("whatever", TextType.TEXT),
+            TextNode("text2", TextType.LINK, "url2")
+        ], split_nodes_link([node]))
+
+    def test_split_nodes_link_case14(self):
+        node = TextNode("whatever[text](url)whatever", TextType.TEXT)
+        self.assertListEqual([
+            TextNode("whatever", TextType.TEXT),
+            TextNode("text", TextType.LINK, "url"),
+            TextNode("whatever", TextType.TEXT)
+        ], split_nodes_link([node]))
 
 
 if __name__ == "__main__":
